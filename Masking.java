de4d6557ecabdd91e554ba6949cc141085322965
qja7767 <qlcjs7789@naver.com>
2022-06-14 11:10:40 +0900

    기밀정보 마스킹(valueOf로검사)

---
+++ Masking.java
@@ -0,0 +1,16 @@
+package com.varyxyz.jv200.mod006;
+
+public class Masking {
+	public static String masking(String phone_number) {
+		char[] ch = phone_number.toCharArray();
+		for( int i = 0; i < ch.length - 4; i ++ ) {
+			ch[i] = '*';
+		}
+		return String.valueOf(ch);
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(masking("01012347667"));
+	}
+}
+
