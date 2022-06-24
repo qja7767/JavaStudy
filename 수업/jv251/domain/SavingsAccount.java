43774bb5137d3e0fc3b2c5e524d3fa0c0ce072b5
LeeSeongHo7984 <qlcjs7789@naver.com>
2022-06-24 10:41:52 +0900

    0624

---
+++ 수업/jv251/domain/SavingsAccount.java
@@ -0,0 +1,35 @@
+package com.varxyz.jv251.domain;
+import com.varxyz.jv251.exception.InsufficientBalanceException;
+
+public class SavingsAccount extends Account {
+	private static final double DEFAULT_INTEREST_RATE = 0.03;
+	private double interestRate;
+	
+	public double getInterestRate() {
+		return interestRate;
+	}
+	public void setInterestRate(double interestRate) {
+		this.interestRate = interestRate;
+	}
+	public static double getDefaultInterestRate() {
+		return DEFAULT_INTEREST_RATE;
+	}
+	public SavingsAccount() {
+		
+	}
+	public SavingsAccount(String accountNum, double balance) {
+		this(accountNum, balance, 0.0); //DEFAULT_INTEREST_RATE
+	}
+	
+	public SavingsAccount(String accountNum, double balance, double interestRate) {
+		super(accountNum, balance);
+		this.interestRate = interestRate;
+	}
+	
+	public void withdraw(double amount) {
+		if(balance - amount < 0 ) {
+			throw new InsufficientBalanceException("잔고 부족");
+		}
+		super.balance -= amount;
+	}
+}
