6681bf1a4ab35f4a45a09111af1e2c35128947d2
LeeSeongHo7984 <qlcjs7789@naver.com>
2022-06-24 10:59:03 +0900

    Create DataSourceManager.java

---
+++ 수업/jv251/dao/DataSourceManager.java
@@ -0,0 +1,11 @@
+package com.varxyz.jv251.dao;
+
+public class DataSourceManager { 
+	private static final String JDBC_URL;
+	private static final String JDBC_USER;
+	private static final String JDBC_PASSWD;
+		
+	static {
+		
+	}
+}
